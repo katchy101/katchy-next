f4694ac9261cbf509f69be1decd6205a033596f0
Alex Katz <alex.katz@fullstack.co.za>
2021-11-17 13:52:51 +0200

    Init Commit

---
+++ lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRw6Hv25YWabP17DQ-c0KwbblsuDWl-Z8",
+  authDomain: "katchy-next.firebaseapp.com",
+  projectId: "katchy-next",
+  storageBucket: "katchy-next.appspot.com",
+  messagingSenderId: "976911179164",
+  appId: "1:976911179164:web:677bfd425728a9c07fbc59",
+  measurementId: "G-GRQM6JM7N3",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
